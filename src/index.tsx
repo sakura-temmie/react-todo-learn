9b09df3c624a576c8d6aadc3b01d9c0a53738be4
sakura miyamoto <sakura.m.208@gmail.com>
2022-08-08 14:58:42 +0900

    login機能作成

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Route, BrowserRouter } from "react-router-dom";
+import Login from "./Login";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+      <>
+        <Route exact path="/" component={App} />
+        <Route exact path="/login" component={Login} />
+      </>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
